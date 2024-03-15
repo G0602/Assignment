52740af62f8be1aa2d343e0c7d7297b2382b687c
G0602 <gowrishan0602@gmail.com>
2024-03-15 21:33:28 +0530

    created a  code to generate a 200x200 grid of dot for canvas and save it as canvas.txt file

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
 
+const int ROWS = 200;
+const int COLS = 200;
 
-	return 0;
+int main() {
+    char array[ROWS][COLS];
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            array[i][j] = '.';
+        }
+    }
+
+    FILE *file = fopen("canvas.txt", "w");
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            fprintf(file, " %c", array[i][j]);
+        }
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+
+    printf("Array successfully saved to canvas.txt.\n");
+
+    return 0;
 }
