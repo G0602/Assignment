e5c63556ad23823b0b9719d16cb1907018343edf
G0602 <gowrishan0602@gmail.com>
2024-03-17 17:36:33 +0530

    moved the typedef to the main.c file from initial.c for optimisation purpose

--- initial/initial.c
+++ initial/initial.c
@@ -3,37 +3,6 @@
 #include "../random/random.h"
 #include "initial.h"
 
-//definig the location of the ships
-typedef struct {
-    int x;
-    int y;
-} point;
-
-
-typedef struct {
-   
-// i'm planig to define a structure called type here
-
-} Type;
-
-// defining the structure of the  battle ships
-typedef struct {
-    point position;
-    char type;// type of B
-    int vMax;
-} btlShp;
-
-// defining the structure of the  escort ships
-typedef struct {
-point position;
-    char type;
-    int angRange;
-    int vMax;
-    int vMin;
-    int angMax;
-    int angMin;
-    char indexNum[7];
-} escShp;
 
 //function to get the initial condition and save it in a text file called initial.txt
 void initialCond(void){
