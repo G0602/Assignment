0d5f17b9e37060d0a83e99e3c37ea1bd5dee5e03
G0602 <gowrishan0602@gmail.com>
2024-03-17 17:49:14 +0530

    moved the structures to a seperate header file named structures.h and included it in the initial.c file

---
+++ structures.h
@@ -0,0 +1,36 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+//definig the location of the ships
+typedef struct {
+    int x;
+    int y;
+} point;
+
+
+typedef struct {
+   
+// i'm planig to define a structure called type here
+
+} Type;
+
+// defining the structure of the  battle ships
+typedef struct {
+    point position;
+    char type;// type of B
+    int vMax;
+} btlShp;
+
+// defining the structure of the  escort ships
+typedef struct {
+point position;
+    char type;
+    int angRange;
+    int vMax;
+    int vMin;
+    int angMax;
+    int angMin;
+    char indexNum[7];
+} escShp;
+
+#endif
