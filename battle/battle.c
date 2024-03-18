e08dbf7dc1875b712444e1f31bda215e0ac5d520
G0602 <gowrishan0602@gmail.com>
2024-03-18 12:15:36 +0530

    created 2 functions name d range and rangeaF to figure out the maximum and the minimun attack range of the E

--- battle/battle.c
+++ battle/battle.c
@@ -3,6 +3,8 @@
 #include"../glbl_vars.h"
 #include "battle.h"
 
+const float g = 9.80665;
+
 int battle(void){
     /*
     FILE *file = fopen("initial.txt", "r");
@@ -13,8 +15,9 @@ int battle(void){
     */
 
     for(int i = 0; i < N; i++){
-    E[i].dist = distCalc(i);
-    printf("The distance between B and %s is -> %f \n", E[i].indexNum, E[i].dist);
+        range(i);
+        E[i].dist = distCalc(i);
+        printf("The distance between B and %s is -> %.2f \n And the maximum and the minimum range are %d,%d\n\n", E[i].indexNum, E[i].dist, E[i].range.max, E[i].range.min);
     }
     
     //fclose(file);
@@ -22,7 +25,23 @@ int battle(void){
     return 0;
 }
 
-float distCalc(int i){
+// this function uses the rangeF function to figure out the maximum and the minimum range of E
+void range(int i){
+    if(fabs(E[i].angMax-45) < (E[i].angMin-45)){
+        E[i].range.max= rangeF(E[i].angMax, E[i].vMax);
+        E[i].range.min= rangeF(E[i].angMin, E[i].vMin);
+    } else {
+        E[i].range.max= rangeF(E[i].angMin, E[i].vMax); 
+        E[i].range.min= rangeF(E[i].angMax, E[i].vMin);
+    }
+}
 
+// this function calclate the distance between B and E
+float distCalc(int i){
     return sqrt(fabs(B.position.x - E[i].position.x) + fabs(B.position.y - E[i].position.y));
+}
+
+//this is a function to calculate the distance travelde by the shell using the angle of the gun and the initial velocity of the shell
+float rangeF(int a, int v){
+    return (float)(pow(v, 2)*sin(2 * a * (M_PI / 180.0)))/g;
 }
--- battle/battle.h
+++ battle/battle.h
@@ -3,5 +3,7 @@
 
 int battle(void);
 float distCalc(int i);
+void range(int i);
+float rangeF(int a, int v);
 
 #endif
