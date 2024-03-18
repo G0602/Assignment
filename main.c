c3a7f4f14b1555c6ece1ddd65c2cd0f699ee1823
G0602 <gowrishan0602@gmail.com>
2024-03-18 16:53:08 +0530

    changed the value of D,n for testing

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "glbl_vars.h"
 
 // defining following variables to be used in all other files and function
-int D = 1000;// Default values for D
+int D = 10;// Default values for D
 int N = 10;// Default values for N
 btlShp B;
 escShp *E;
