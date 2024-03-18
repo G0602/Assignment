27d41cd448994eda70fd8c6243f005f03f9e8838
G0602 <gowrishan0602@gmail.com>
2024-03-18 16:58:09 +0530

    corrected some mistakes

--- battle/battle.c
+++ battle/battle.c
@@ -46,10 +46,10 @@ void rangeMinMax(int i){
 
 // this function calclate the distance between B and E
 float distCalc(int i){
-    return sqrt(fabs(B.position.x - E[i].position.x) + fabs(B.position.y - E[i].position.y));
+    return sqrt(pow((B.position.x - E[i].position.x),2) + pow((B.position.y - E[i].position.y),2));
 }
 
 //this is a function to calculate the distance travelde by the shell using the angle of the gun and the initial velocity of the shell
 float rangeF(int a, int v){
-    return (pow(v, 2)*sin(2 * a * (M_PI / 180.0)))/(g * 1000); // this will give the range in kilo meters
+    return ((pow(v, 2)*sin(2 * a * (M_PI / 180.0)))/(g * 1000)); // this will give the range in kilo meters
 }
