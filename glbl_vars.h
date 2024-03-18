aec27f472a98eab29e8a9192a0741cc362cfe77e
G0602 <gowrishan0602@gmail.com>
2024-03-18 13:23:59 +0530

    added more coments for clarity

--- glbl_vars.h
+++ glbl_vars.h
@@ -19,7 +19,7 @@ typedef struct {
 typedef struct {
     Point position;// coordinate of the B in the canvas
     char type;// type of B
-    int vMax;// maximum velocity of B's shell
+    int vMax;// maximum velocity of B's shell, will be in meter per second
     bool status;//if sgttus is 1 the ship is fine if it's 0 the the ship is destroyed
 } btlShp;
 
@@ -27,19 +27,19 @@ typedef struct {
 typedef struct {
     Point position;// coordinate of the E in the canvas
     char type; // type of the E
-    int vMax;// maximum velocity of E's shell
-    int vMin;// minimum velocity of E's shell
-    int angRange; //the vertical range of the E
-    int angMax;// maximum angle of E's gun
-    int angMin;// minimum angle of E's gun
-    Minmax range; // attack range of E
+    int vMax;// maximum velocity of E's shell, will be in meter per second
+    int vMin;// minimum velocity of E's shell, will be in meter per second
+    int angRange; //the vertical range of the E, in degree
+    int angMax;// maximum angle of E's gun, in degree
+    int angMin;// minimum angle of E's gun, in degree
+    Minmax range; // attack range of E, in kilo meter
     float dist;// distance from the B
     bool status;//if status is 1 the ship is fine if it's 0 the the ship is destroyed
     char indexNum[7];// for the unique ship identifiyer
 } escShp;
 
 extern int N; // N is for the number of escort ships
-extern int D; // D is for the size of the battle field
+extern int D; // D is for the size of the battle field, in kilo meter
 
 extern btlShp B; //B is to indicate the battle ship
 extern escShp *E; //an escShp class pointer to be used as a pointer
