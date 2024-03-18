fc8cef6aa8a26efbb282577fc46552517d2631ee
G0602 <gowrishan0602@gmail.com>
2024-03-18 21:15:13 +0530

    created canBAtk and atkTime functions and variables needed for that and added min and max range foe B and declared the functions in the header file

--- battle/battle.c
+++ battle/battle.c
@@ -4,7 +4,9 @@
 #include "battle.h"
 #include "../initial/initial.h"
 
-const float g = 9.80665;
+const float g = 9.80665;// acceleration of gravity
+int kill = 0;//to show how many Es' has been destroyed by B
+float time = 0;// total time to complete B's attacks
 
 int battle(void){
     /*
@@ -14,11 +16,16 @@ int battle(void){
         return 1;
     }
     */
+    B.range.min= 0;
+    B.range.max= rangeF(45, B.vMax);
 
     for(int i = 0; i < N; i++){
         rangeMinMax(i);
         E[i].dist = distCalc(i);
         canEAtk(i);
+        if (B.status == 1){
+            canBAtk (i);
+        }
     }
     
     //fclose(file);
@@ -55,6 +62,11 @@ float rangeF(int a, int v){
     return ((pow(v, 2)*sin(2 * a * (M_PI / 180.0)))/(g * 1000)); // this will give the range in kilo meters
 }
 
+//this a function to calculate the time to shell reach E
+float atkTime(int i){
+    return ((2 * B.vMax * sin(0.5 * asin((E[i].dist * g)/pow(B.vMax, 2))))/g);
+}
+
 
 //this function check whether the E can attack B
 void canEAtk(int i){
@@ -64,6 +76,20 @@ void canEAtk(int i){
             printf("B is destroyed.\nIt has been attcked by following ships:\n");
             B.status = 0;
         }
+        printf("%s\n", E[i].indexNum);
+    }
+}
+
+//this function check whether the B can attack E
+void canBAtk(int i){
+    if(E[i].dist <= B.range.max){
+        if (kill == 0){ 
+            printf("Following ships has been destroyed by B:\n");
+        }
+        E[i].status = 0;
+        kill++;
+        time += atkTime(i);
+        E[i].status = 0;
         printf("%s\n", E[i].indexNum);
     }
 }
--- battle/battle.h
+++ battle/battle.h
@@ -6,5 +6,7 @@ float distCalc(int i);
 void rangeMinMax(int i);
 float rangeF(int a, int v);
 void canEAtk(int i);
+void canBAtk(int i);
+float atkTime(int i);
 
 #endif
