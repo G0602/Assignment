02c71eb05b96aa8470478a3397beb085bf8a3ce6
G0602 <gowrishan0602@gmail.com>
2024-03-19 12:42:20 +0530

    created this folder to contain the functions related to movements and locations

---
+++ canvas/canvas.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "glbl_vars.h"
+#include "random.h"
+
+
+
+/*
+int canvas() {
+    char array[D][D];
+
+    for (int i = 0; i < D; i++) {
+        for (int j = 0; j < COLS; j++) {
+            array[i][j] = '.';
+        }
+    }
+
+
+
+    FILE *file = fopen("canvas.txt", "w");
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            fprintf(file, " %c", array[i][j]);
+        }
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+
+    printf("Array successfully saved to canvas.txt.\n");
+
+    return 0;
+}
+*/
+
+void location(int k){
+    for(int i = 0;i < k; i++){
+        movement[i].position.x = ranNum(0,D);
+        movement[i].position.y = ranNum(0,D);
+    }
+}
+
+// this function calclate the distance between B and E
+float distCalc(int x1, int y1, int x2, int y2){
+    return sqrt(pow((x1 - x2),2) + pow((y1 - y2),2));
+}
+
+void chose(){
+
+}
+
+void move(){
+
+}
---
+++ canvas/canvas.h
@@ -0,0 +1,7 @@
+#ifndef CANVAS_H
+#define CANVAS_H
+
+
+float distCalc(int x1, int y1, int x2, int y2);
+
+#endif
